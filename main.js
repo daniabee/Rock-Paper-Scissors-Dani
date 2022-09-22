01cdfb54952bffca243762dce06413b14c73c0df
Dani Bagley <bagley.a.danielle@gmail.com>
2022-09-22 10:12:32 -0600

    Create working form

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
+var playerForm = document.querySelector('.player-form-side');
+var submitButton = document.querySelector('#submit');
+var playerInput = document.querySelector('#player-input');
+var playerName = document.querySelector('#player-name');
+var playerSide = document.querySelector('.player-side');
+
 var uniqueGameOption = document.querySelector('.unique-game');
 var classicGameOption = document.querySelector('.classic-game');
+
 var mainView = document.querySelector('.main-view');
 var classicGameView = document.querySelector('.classic-view');
 var uniqueGameView = document.querySelector('.unique-view');
+
 var homeButton1 = document.querySelectorAll('.home')[0];
 var homeButton2 = document.querySelectorAll('.home')[1];
 
 
 //EVENT LISTENERS
+submitButton.addEventListener('click', submitPlayerInfo);
 uniqueGameOption.addEventListener('click', goToClassicView);
 classicGameOption.addEventListener('click', goToUniqueView);
 homeButton1.addEventListener('click', goHomeView);
 homeButton2.addEventListener('click', goHomeView);
 
 //FUNCTIONS
+function submitPlayerInfo() {
+  if (playerInput.value === '') {
+    try {
+      throw new Error('Whoops! Add a name, please.');
+    } catch (e) {
+      console.error(`${e.name}: ${e.message}`);
+    }
+  } else {
+    playerForm.classList.add('hidden');
+    playerSide.classList.remove('hidden');
+    playerName.innerText = `${playerInput.value}`;
+  }
+}
+
 function goToClassicView() {
   mainView.classList.add('hidden');
   uniqueGameView.classList.remove('hidden');
